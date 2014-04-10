ef6ed51ced368306e429d4518c0759ab8e0eed7d
Rick Hall <rickhall2000@hotmail.com>
2014-04-10 10:38:46 -0400

    read 10.3 - agents

--- src/joy/ch10.clj
+++ src/joy/ch10.clj
@@ -108,3 +108,62 @@
       (Thread/sleep 10)
       (dosync (alter r inc)))
     :done))
+
+;; 10.3 - agents
+
+(def joy (agent []))
+(send joy conj "first edition")
+(defn slow-conj [coll item]
+  (Thread/sleep 1000)
+  (conj coll item))
+
+(send joy slow-conj "Second edition")
+
+(def log-agent (agent 0))
+
+(defn do-log [msg-id message]
+  (println msg-id ":" message)
+  (inc msg-id))
+
+(defn do-step [channel message]
+  (Thread/sleep 1)
+  (send-off log-agent do-log (str channel message)))
+
+(defn three-step [channel]
+  (do-step channel " ready to begin (step 0)")
+  (do-step channel " warming up (step 1)")
+  (do-step channel " really getting going now (step 2)")
+  (do-step channel " done (step 3)"))
+
+(defn all-together-now []
+  (dothreads! #(three-step "alpha"))
+  (dothreads! #(three-step "beta"))
+  (dothreads! #(three-step "gamma")))
+
+(defn exercise-agents [send-fn]
+  (let [agents (map #(agent %) (range 10))]
+    (doseq [a agents]
+      (send-fn a (fn [_] (Thread/sleep 1000))))
+    (doseq [a agents]
+      (await a))))
+
+(time (exercise-agents send-off))
+(time (exercise-agents send))
+
+(send log-agent (fn [] 2000))
+
+@log-agent
+
+(agent-error log-agent)
+(send log-agent (fn [_] 3000))
+
+(restart-agent log-agent 2500 :clear-actions true)
+
+(send-off log-agent do-log "The agent lives")
+
+(defn handle-log-error [the-agent the-err]
+  (println "an action sent to the log-agent threw" the-err))
+
+(set-error-handler! log-agent handle-log-error)
+
+(set-error-mode! log-agent :continue)
