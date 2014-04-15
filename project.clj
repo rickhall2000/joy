bbe544cf06b6d2a6b6325a8156484105a277f3d5
Rick Hall <rickhall2000@hotmail.com>
2014-04-15 15:20:11 -0400

    updated description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject joy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "scratch pad as I work through the joy of clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
