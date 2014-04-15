039a1a75a148fa96e877cefe5c5b0ca737b34505
Rick Hall <rick@rickhallcodes.net>
2014-04-15 15:11:19 -0400

    only got part way throgh 10.1, need to debug

--- src/joy/ch11.clj
+++ src/joy/ch11.clj
@@ -1,1 +1,43 @@
-(ns joy.ch11)
+(ns joy.ch11
+  (:require (clojure [xml :as xml]))
+  (:require (clojure [zip :as zip]))
+  (:import (java.util.regex Pattern)))
+(time (let [x (future (do (Thread/sleep 5000)
+                          (+ 41 1)))]
+        [@x @x]))
+
+(defn feed->zipper [uri-str]
+  (->> xml/parse uri-str)
+  zip/xml-zip)
+
+(defn normalize [feed]
+  (if (= :feed (:tag (first feed)))
+    feed
+    (zip/down feed)))
+
+(defn feed-children [uri-str]
+  (->> uri-str
+       feed->zipper
+       normalize
+       zip/children
+       (filter (comp #{:item :entry} :tag))))
+
+(defn title [entry]
+  (some->> entry
+           :content
+           (some #(when (= :title (:tag %)) %))
+           :content
+           first))
+
+(defn count-text-task [extractor txt feed]
+  (let [items (feed-children feed)
+        re (Pattern/compile (str "(?i" txt ))]
+    (->> items
+         (map extractor)
+         (mapcat #(re-seq re %))
+         count)))
+
+(count-text-task
+ title
+ "Erlang"
+ "http://feeds.feedburner.com/ElixirLang")
