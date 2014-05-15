3d8dc71ff37f42749ebce2d9bb7fe3eb1863b118
Rick Hall <rickhall2000@hotmail.com>
2014-05-15 08:48:24 -0400

    Finished reading 12.1

--- src/joy/ch12.clj
+++ src/joy/ch12.clj
@@ -57,3 +57,63 @@
       (respond exchange (prn-str headers)))))
 
 (update-proxy p {"handle" echo-handler})
+
+(defn html-around [o]
+  (proxy [FilterOutputStream]
+      [o]
+    (write [raw-bytes]
+      (proxy-super write
+                   (.getBytes (str "<html><body"
+                                   (String. raw-bytes)
+                                   "</body></html>"))))))
+
+(defn listing [file]
+  (-> file .list sort))
+
+(listing (io/file "."))
+
+(listing (io/file "./README.md"))
+
+(defn html-links [root filenames]
+      (string/join
+       (for [file filenames]
+         (str "<a href='"
+              (str root
+                   (if (= "/" root)
+                     ""
+                     File/separator)
+                   file)
+              "'>"
+              file "</a><br>"))))
+
+(html-links "." (listing (io/file ".")))
+
+(defn details [file]
+  (str (.getName file) " is "
+       (.length file) " bytes"))
+
+(details (io/file "./README.md"))
+
+(defn uri->file [root uri]
+  (->> uri
+       str
+       URLDecoder/decode
+       (str root)
+       io/file))
+
+(uri->file "." (URI. "/project.clj"))
+(details (uri->file "." (URI. "/project.clj")))
+
+(def fs-handler
+  (fn [_ exchange]
+    (let [uri (.getRequestURI exchange)
+          file (uri->file "." uri)]
+      (if (.isDirectory file)
+        (do (.add (.getResponseHeaders exchange)
+                  "Content-Type" "text/html")
+            (respond html-around
+                     exchange
+                     (html-links (str uri) (listing file))))
+        (respond exchange (details file))))))
+
+(update-proxy p {"handle" fs-handler})
