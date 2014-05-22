23c951e989c319d174bda1b5a681962ab88409c2
Rick Hall <rickhall2000@hotmail.com>
2014-05-22 08:55:22 -0400

    more progress on 13.2

--- project.clj
+++ project.clj
@@ -4,4 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1835"]])
+                 [org.clojure/clojurescript "0.0-1835"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src/cljs"]
+     :compiler
+     {:output-to "dev-target/all.js"
+      :optimizations :whitespace
+      :pretty-print true}}]})
